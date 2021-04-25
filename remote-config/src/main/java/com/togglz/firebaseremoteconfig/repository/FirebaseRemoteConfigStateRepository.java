9bef4ac2c017989cfffd6dd6d827d0d5869f7376
Yamil Medina <yamilmedina@users.noreply.github.com>
2021-04-24 20:06:37 -0400

    feature(remoteconfig): add base module and staterepository impl skeleton

---
+++ remote-config/src/main/java/com/togglz/firebaseremoteconfig/repository/FirebaseRemoteConfigStateRepository.java
@@ -0,0 +1,18 @@
+package com.togglz.firebaseremoteconfig.repository;
+
+import org.togglz.core.Feature;
+import org.togglz.core.repository.FeatureState;
+import org.togglz.core.repository.StateRepository;
+
+public class FirebaseRemoteConfigStateRepository implements StateRepository {
+
+    @Override
+    public FeatureState getFeatureState(Feature feature) {
+        return null;
+    }
+
+    @Override
+    public void setFeatureState(FeatureState featureState) {
+
+    }
+}
